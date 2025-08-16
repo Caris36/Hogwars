4b3136efe49a650402e4d522c0e2162d95771988
Caris36 <Karisyeosu@gmail.com>
2025-08-16 11:31:24 +0800

    Cameras pmo

---
+++ Hogwars/3. Live Camera Stuff/FrameHandler.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Hogwars
+//
+//  Created by Karis on 16/8/25.
+//
+
---
+++ Hogwars/3. Live Camera Stuff/FrameView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameView.swift
+//  Hogwars
+//
+//  Created by Karis on 16/8/25.
+//
+
+import SwiftUI
+
+struct FrameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameView()
+}
