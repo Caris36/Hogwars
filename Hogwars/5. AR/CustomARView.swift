63221b4c5fdcbb517cca0b48c429403e3bb9dcb0
Hailey <tanzhailey@gmail.com>
2025-08-23 10:59:41 +0800

    finished setting up basic ar and added functions for placing a blue block and adding other entities

--- Hogwars/5. AR/CustomARView.swift
+++ Hogwars/5. AR/CustomARView.swift
@@ -17,5 +17,59 @@ class CustomARView: ARView {
     }
     convenience init(){
         self.init(frame: UIScreen.main.bounds)
+        placeBlueBlock()
+    }
+    func configurationExamples(){
+        //tracks the device relative to the environment
+        let configuration = ARWorldTrackingConfiguration()
+        session.run(configuration)
+        
+//        //tracks wrt global coordinates (possibly not supported)
+//        let _ = ARGeoTrackingConfiguration()
+        
+        //tracks faces in the scene
+        let _ = ARFaceTrackingConfiguration()
+        //tracks bodies in the scene
+        let _ = ARBodyTrackingConfiguration()
+    }
+    func anchorExamples(){
+        //attach anchors at specific coordinates in the iphone centered coordinate system
+        let coordinateAnchor = AnchorEntity(world: .zero)
+        
+        //attach anchors to detected planes (works best with devices LIDAR sensor)
+        let _ = AnchorEntity(plane: .horizontal)
+        let _ = AnchorEntity(plane: .vertical)
+        
+        //attach anchors to tracked body parts
+        let _ = AnchorEntity(.face)
+        
+//        //attach anchors to tracked images, such as markers and visual codes
+//        let _ = AnchorEntity(.image(group: <#T##String#>, name: <#T##String#>))
+        
+        //add an anchor to the scene
+        scene.addAnchor(coordinateAnchor)
+    }
+    func entityExamples(){
+        //load an entity from a usdz file
+        let _ = try? Entity.load(named: "usdzFileName")
+        
+        //load entity from a reality file, animations and interactive
+        let _ = try? Entity.load(named: "realityFileName")
+        
+        //generate entity with code, like for super simple
+        let box = MeshResource.generateBox(size: 1)
+        let entity = ModelEntity(mesh: box)
+        
+        //add entity to an anchor so its placed in the scene
+        let anchor = AnchorEntity()
+        anchor.addChild(entity)
+    }
+    func placeBlueBlock(){
+        let block = MeshResource.generateBox(size: 1)
+        let material = SimpleMaterial(color: .blue, isMetallic: false)
+        let entity = ModelEntity(mesh: block, materials: [material])
+        let anchor = AnchorEntity(plane: .horizontal)
+        anchor.addChild(entity)
+        scene.addAnchor(anchor)
     }
 }
