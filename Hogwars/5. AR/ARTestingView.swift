7dd5d1a62e0e624977f253b6d5431f69e6835692
Hailey <tanzhailey@gmail.com>
2025-08-23 10:13:12 +0800

    created an AR folder with AR views

---
+++ Hogwars/5. AR/ARTestingView.swift
@@ -0,0 +1,18 @@
+//
+//  ARTestingView.swift
+//  Hogwars
+//
+//  Created by Eugene Tan on 23/8/25.
+//
+
+import SwiftUI
+
+struct ARTestingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARTestingView()
+}
